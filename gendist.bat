176539e080cf1bf490fa9c474e7bee642b48bd99
LiangShuang <liangshuang@sutu.tech>
2020-07-22 13:33:00 +0800

    script to generate exe

---
+++ gendist.bat
@@ -0,0 +1,8 @@
+@echo off
+
+ROBOCOPY .\node_modules .\dist\node_modules *.node *.dll /S
+move dist\node_modules\gl\build\Release dist\build
+rd dist\node_modules\gl /s /q
+
+call nexe cli.js
+move cli.exe dist\generate.exe
